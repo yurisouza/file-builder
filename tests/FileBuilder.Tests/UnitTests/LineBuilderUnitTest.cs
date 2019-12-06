43c93e696c8dd780f7ee95400895ea6172624a33
Yuri Souza <yuri.souza@worthix.com>
2019-12-06 16:27:07 -0300

    :white_check_mark: Inserts new tests

--- tests/FileBuilder.Tests/UnitTests/LineBuilderUnitTest.cs
+++ tests/FileBuilder.Tests/UnitTests/LineBuilderUnitTest.cs
@@ -72,8 +72,92 @@ namespace FileBuilder.Tests.UnitTests
             validateTest.Should().BeTrue();
         }
 
-        [Fact(DisplayName = "Given I have a new line When I check it Then I should have a line with current position 0")]
+        [Fact(DisplayName = "Given I have any text When I add in the line Then I should have added the text on the specific column.")]
         [Trait("LineBuilder", "AddText()")]
+        public void Should_Be_Add_Text_In_Specific_Column_Success()
+        {
+            //Arrange
+            var line = new LineBuilder();
+            var text = FakerPrimitiveTypes.GetSampleText(0, 10000);
+            line.AddText(2, text);
+
+            //Act
+            var validateTest = line.GetText(2) == text;
+
+            //Assert
+            validateTest.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Given I have a list of texts When I add in the line Then I should have added the texts sequentially.")]
+        [Trait("LineBuilder", "AddTexts()")]
+        public void Should_Be_Add_Texts_Sequentially_Using_List_Success()
+        {
+            //Arrange
+            var line = new LineBuilder();
+
+            IEnumerable<string> texts = new List<string>()
+            {
+                FakerPrimitiveTypes.GetSampleText(0, 10000),
+                FakerPrimitiveTypes.GetSampleText(0, 10000),
+                FakerPrimitiveTypes.GetSampleText(0, 10000),
+                FakerPrimitiveTypes.GetSampleText(0, 10000)
+            };
+
+            line.AddTexts(texts);
+
+            //Act
+            var validateTest = line.GetCurrentPosition() == texts.Count()
+                                && texts.Select((text, index) => new { index, text })
+                                        .All(data => data.index == line.GetPosition(data.text));
+
+            //Assert
+            validateTest.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Given I have some texts or only a text When I add in the line Then I should have added the texts sequentially.")]
+        [Trait("LineBuilder", "AddTexts()")]
+        public void Should_Be_Add_Texts_Sequentially_Using_Params_Success()
+        {
+            //Arrange
+            var line = new LineBuilder();
+
+            var firstText = FakerPrimitiveTypes.GetSampleText(0, 10000);
+            var secondText = FakerPrimitiveTypes.GetSampleText(0, 10000);
+
+            line.AddTexts(firstText, secondText);
+
+            //Act
+            var validateTest = line.GetCurrentPosition() == 2
+                                && line.GetPosition(firstText) == 0
+                                && line.GetPosition(secondText) == 1;
+
+            //Assert
+            validateTest.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Given I have some texts or only a text When I add in the line Then I should have added the texts sequentially with the word specific before and after.")]
+        [Trait("LineBuilder", "AddTextsConcat()")]
+        public void Should_Be_Add_Texts_Concat_Sequentially_Success()
+        {
+            //Arrange
+            var line = new LineBuilder();
+
+            var firstText = FakerPrimitiveTypes.GetSampleText(0, 10000);
+            var secondText = FakerPrimitiveTypes.GetSampleText(0, 10000);
+
+            line.AddTextsConcat("before-", "-after", firstText, secondText);
+
+            //Act
+            var validateTest = line.GetCurrentPosition() == 2
+                                && line.GetPosition($"before-{firstText}-after") == 0
+                                && line.GetPosition($"before-{secondText}-after") == 1;
+
+            //Assert
+            validateTest.Should().BeTrue();
+        }
+
+        [Fact(DisplayName = "Given I have a new line When I check it Then I should have a line with current position 0")]
+        [Trait("LineBuilder", "LineBuilder()")]
         public void Should_Be_Current_Position_0_Without_Parameter()
         {
             //Arrange
@@ -84,7 +168,7 @@ namespace FileBuilder.Tests.UnitTests
         }
 
         [Theory(DisplayName = "Given I have a new line already with text When I check it Then I should have a line with current position equal amount of added texts minus 1.")]
-        [Trait("LineBuilder", "AddText()")]
+        [Trait("LineBuilder", "LineBuilder()")]
         [InlineData(1)]
         [InlineData(5)]
         [InlineData(10)]
